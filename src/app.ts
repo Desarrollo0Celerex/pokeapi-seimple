e536192c133a7bf7fe1b0eb87bf320fbf60f593d
Luis Angel Sevilla Celerex <luissevillajobcontactme111@gmail.com>
2025-01-20 15:02:06 -0600

    now indeed canging title

--- src/app.ts
+++ src/app.ts
@@ -87,7 +87,7 @@ app.get("/", async (req: Request, res: Response) => {
         </style>
       </head>
       <body>
-        <h1>Pokédex chingona TS another one</h1>
+        <h1>Pokédex Pipeline 3</h1>
         <div>
           ${pokemonList}
         </div>
