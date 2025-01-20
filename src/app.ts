04ad223367203703027c4997377e470d472882d2
Luis Angel Sevilla Celerex <luissevillajobcontactme111@gmail.com>
2025-01-20 15:04:16 -0600

    V0.4

--- src/app.ts
+++ src/app.ts
@@ -87,7 +87,7 @@ app.get("/", async (req: Request, res: Response) => {
         </style>
       </head>
       <body>
-        <h1>Pokédex Pipeline 3</h1>
+        <h1>Pokédex Pipeline 4</h1>
         <div>
           ${pokemonList}
         </div>
